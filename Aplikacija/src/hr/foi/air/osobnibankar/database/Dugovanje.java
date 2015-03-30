2069c1066c2d8f1614b623278c922f4727cb67a9
TihanaB <tibusetin@foi.hr>
2015-03-30 22:26:05 +0200

    unos
    PiR i PiD

--- Aplikacija/src/hr/foi/air/osobnibankar/database/Dugovanje.java
+++ Aplikacija/src/hr/foi/air/osobnibankar/database/Dugovanje.java
@@ -1,47 +1,45 @@
 package hr.foi.air.osobnibankar.database;
 
-import java.util.Date;
-
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 
-@Table (name = "Dugovanje")
-public class Dugovanje extends Model{
-	@Column (name = "naziv")
+@Table(name = "Dugovanje")
+public class Dugovanje extends Model {
+	@Column(name = "naziv")
 	public String naziv;
-	@Column (name = "opis")
+	@Column(name = "opis")
 	public String opis;
-	@Column (name = "iznos")
+	@Column(name = "iznos")
 	public Double iznos;
-	@Column (name = "datum")
-	public Date datum;
-	
-	public Dugovanje () {
+	@Column(name = "datum")
+	public String datum;
+
+	public Dugovanje() {
 		super();
 	}
-	
-	public Dugovanje (String naziv, String opis, Double iznos, Date datum){
+
+	public Dugovanje(String naziv, String opis, Double iznos, String datum) {
 		this.naziv = naziv;
 		this.opis = opis;
 		this.iznos = iznos;
 		this.datum = datum;
 	}
-	
-	public String getNaziv(){
-		return naziv;		
+
+	public String getNaziv() {
+		return naziv;
 	}
-	
-	public String getOpis(){
-		return naziv;		
+
+	public String getOpis() {
+		return naziv;
 	}
-	
-	public Double getIznos(){
-		return iznos;		
+
+	public Double getIznos() {
+		return iznos;
 	}
-	
-	public Date getDatum(){
-		return datum;		
+
+	public String getDatum() {
+		return datum;
 	}
-	
+
 }
--- Aplikacija/src/hr/foi/air/osobnibankar/database/Rashod.java
+++ Aplikacija/src/hr/foi/air/osobnibankar/database/Rashod.java
@@ -1,47 +1,45 @@
 package hr.foi.air.osobnibankar.database;
 
-import java.util.Date;
-
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 
-@Table (name = "Rashod")
-public class Rashod extends Model{
-	@Column (name = "naziv")
+@Table(name = "Rashod")
+public class Rashod extends Model {
+	@Column(name = "naziv")
 	public String naziv;
-	@Column (name = "opis")
+	@Column(name = "opis")
 	public String opis;
-	@Column (name = "iznos")
+	@Column(name = "iznos")
 	public Double iznos;
-	@Column (name = "datum")
-	public Date datum;
-	
-	public Rashod () {
+	@Column(name = "datum")
+	public String datum;
+
+	public Rashod() {
 		super();
 	}
-	
-	public Rashod (String naziv, String opis, Double iznos, Date datum){
+
+	public Rashod(String naziv, String opis, Double iznos, String datum) {
 		this.naziv = naziv;
 		this.opis = opis;
 		this.iznos = iznos;
 		this.datum = datum;
 	}
-	
-	public String getNaziv(){
-		return naziv;		
+
+	public String getNaziv() {
+		return naziv;
 	}
-	
-	public String getOpis(){
-		return naziv;		
+
+	public String getOpis() {
+		return naziv;
 	}
-	
-	public Double getIznos(){
-		return iznos;		
+
+	public Double getIznos() {
+		return iznos;
 	}
-	
-	public Date getDatum(){
-		return datum;		
+
+	public String getDatum() {
+		return datum;
 	}
-	
+
 }
